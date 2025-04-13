20360e290414fd131e92864841f9ead233eff726
Maxs28 <maxsilcolon140@gmail.com>
2025-04-13 01:55:08 -0400

    Add track Mouse

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const container = document.querySelector(".carrouselInside");
-
 setInterval(() => {
     container.style.transition = "transform 1s ease-in-out";
     container.style.transform = "translateX(299px)";
@@ -10,3 +9,11 @@ setInterval(() => {
         container.style.transform = "translateX(0)"; 
     }, 890);
 },1050);
+
+const light = document.querySelector('.light-overlay');
+
+document.addEventListener('mousemove', (e) => {
+  const x = e.clientX;
+  const y = e.clientY;
+  light.style.background = `radial-gradient(circle at ${x}px ${y}px, rgba(28, 153, 173, 0.15) 0%, transparent 30%)`;
+});
