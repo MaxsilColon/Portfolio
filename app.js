6d52fb239f812d33a29a442d1e9e361b75c6290f
Maxs28 <maxsilcolon140@gmail.com>
2025-04-13 22:43:50 -0400

    Add Animation to Portfolio

--- app.js
+++ app.js
@@ -1,19 +1,38 @@
 const container = document.querySelector(".carrouselInside");
 setInterval(() => {
-    container.style.transition = "transform 1s ease-in-out";
-    container.style.transform = "translateX(299px)";
+    container.style.transition = "transform 2s ease-in-out";
+    container.style.transform = "translateX(225px)";
 
     setTimeout(() => {
         container.style.transition = "none"; 
         container.prepend(container.lastElementChild); 
         container.style.transform = "translateX(0)"; 
-    }, 890);
-},1050);
+    }, 1500);
+},2000);
 
 const light = document.querySelector('.light-overlay');
 
 document.addEventListener('mousemove', (e) => {
   const x = e.clientX;
   const y = e.clientY;
-  light.style.background = `radial-gradient(circle at ${x}px ${y}px, rgba(28, 153, 173, 0.15) 0%, transparent 30%)`;
+  light.style.background = `radial-gradient(circle at ${x}px ${y}px, rgba(31, 132, 249, 0.15) 0%, transparent 40%)`;
 });
+
+const sectionProject = document.querySelector(".sectionProject")
+
+const cardProject = document.querySelectorAll(".cardProject")
+const observer = new IntersectionObserver((entries)=>{
+  entries.forEach(entry=>{
+    if(entry.isIntersecting){
+      cardProject.forEach((entry, index)=>{
+        entry.style.animationDelay = `${index * 0.5}s`
+        entry.classList.add("animationProjectCard")
+      })
+      observer.unobserve(entry.target)
+    }
+  })
+},{
+  threshold: 0.5
+})
+
+observer.observe(sectionProject)
